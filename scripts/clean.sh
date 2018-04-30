7368fcd424cb8f712e9b9fbaeaa97f2ccd18d08d
Nick DeFazio <ndefazio@kenzan.com>
2018-04-30 17:04:16 -0400

    Updated deployments and start up/tear down scripts for istio

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 kubectl delete services/title-middle
-kubectl delete deployments --all
-kubectl delete pods --all
+kubectl delete deployments/title-middle
+kubectl delete deployments/title-edge
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,2 +1,19 @@
-minikube start
-eval $(minikube docker-env)
+echo 'Starting Minikube...'
+minikube start --extra-config=controller-manager.ClusterSigningCertFile="/var/lib/localkube/certs/ca.crt" --extra-config=controller-manager.ClusterSigningKeyFile="/var/lib/localkube/certs/ca.key" --extra-config=apiserver.Admission.PluginNames=NamespaceLifecycle,LimitRanger,ServiceAccount,PersistentVolumeLabel,DefaultStorageClass,DefaultTolerationSeconds,MutatingAdmissionWebhook,ValidatingAdmissionWebhook,ResourceQuota --kubernetes-version=v1.9.0
+
+##eval $(minikube docker-env)
+
+echo 'Installing base istio...'
+kubectl apply -f istio/istio-0.7.1/install/kubernetes/istio.yaml
+
+echo 'Configuring for automatic sidecar injection...'
+echo 'Adding certs'
+./istio/istio-0.7.1/install/kubernetes/webhook-create-signed-cert.sh --service istio-sidecar-injector --namespace istio-system --secret sidecar-injector-certs
+echo 'Adding sidecar injection configmap'
+kubectl apply -f istio/istio-0.7.1/install/kubernetes/istio-sidecar-injector-configmap-release.yaml
+echo 'Configuring webhook'
+cat istio/istio-0.7.1/install/kubernetes/istio-sidecar-injector.yaml |./istio/istio-0.7.1/install/kubernetes/webhook-patch-ca-bundle.sh > istio/istio-0.7.1/install/kubernetes/istio-sidecar-injector-with-ca-bundle.yaml
+echo 'Installing webhook'
+kubectl apply -f istio/istio-0.7.1/install/kubernetes/istio-sidecar-injector-with-ca-bundle.yaml
+
+export PATH=$PWD/istio/istio-0.7.1/bin:$PATH
